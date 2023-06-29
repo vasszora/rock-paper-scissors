75b184d5796bb99a81ea2b8afe4240a345ba570d
vasszora <vasszora@gmail.com>
2023-06-29 11:23:48 +0200

    Handle invalid hand input

--- rock-paper-scissors/src/main/java/com/example/rockpaperscissors/controller/PlayController.java
+++ rock-paper-scissors/src/main/java/com/example/rockpaperscissors/controller/PlayController.java
@@ -1,10 +1,12 @@
 package com.example.rockpaperscissors.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.example.rockpaperscissors.model.Hand;
 import com.example.rockpaperscissors.model.Result;
@@ -18,14 +20,23 @@ public class PlayController {
     private RuleService ruleService;
 
     @PostMapping
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
     public Result play(@RequestParam(required = false) boolean explain,
-            @RequestParam(name = "userHand") Hand userHand) {
+            @RequestParam String userHand) {
         Hand computerHand = Hand.randomHand();
-        Rule rule = ruleService.getRuleByName(userHand);
-        boolean userWon = (rule.getBeats().equals(computerHand));
-        if (explain) {
-            return new Result(userWon, computerHand, userHand, rule);
+        Hand userHandEnum;
+        try {
+            userHandEnum = Hand.valueOf(userHand.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid hand: " + userHand);
         }
-        return new Result(userWon, computerHand, userHand);
+
+        Rule rule = ruleService.getRuleByName(userHandEnum);
+        boolean userWon = (rule.getBeats().equals(computerHand));
+
+        if (explain) {
+            return new Result(userWon, computerHand, userHandEnum, rule);
+        }
+        return new Result(userWon, computerHand, userHandEnum);
     }
 }
